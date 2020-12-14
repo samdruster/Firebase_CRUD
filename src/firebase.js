61d503f046696f8efeee1429efa0045d964a1e23
samdruster <samdruster@gmail.com>
2020-12-15 00:59:54 +0530

    init

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyC_my0AoLeMUKGJZjzVYlJeU3KodYEhyIw",
+    authDomain: "react-crud-352e6.firebaseapp.com",
+    databaseURL: "https://react-crud-352e6-default-rtdb.firebaseio.com",
+    projectId: "react-crud-352e6",
+    storageBucket: "react-crud-352e6.appspot.com",
+    messagingSenderId: "1095180948584",
+    appId: "1:1095180948584:web:b41775dac5467f985a57dd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
